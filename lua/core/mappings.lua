602ea5cfb5889cda2e460cfba7de8872e2878d73
Jacob Corum <jacob.w.corum@gmail.com>
2022-07-27 21:23:24 -0600

    mappings update

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -192,56 +192,58 @@ maps.t["<C-j>"] = { "<c-\\><c-n><c-w>j", desc = "Terminal down window navigation
 maps.t["<C-k>"] = { "<c-\\><c-n><c-w>k", desc = "Terminal up window navigation" }
 maps.t["<C-l>"] = { "<c-\\><c-n><c-w>l", desc = "Terminal right window naviation" }
 
-maps.n['zb'] = { function () require('fzf-lua').buffers() end, desc = 'fzf buffers' }
-maps.n['zp'] = { function () require('fzf-lua').files() end, desc = 'fzf files' }
-maps.n['zh'] = { function () require('fzf-lua').oldfiles() end, desc = 'fzf history' }
-maps.n['zq'] = { function () require('fzf-lua').quickfix() end, desc = 'fzf quickfix' }
-maps.n['zll'] = { function () require('fzf-lua').loclist() end, desc = 'fzf loclist' }
-maps.n['zl'] = { function () require('fzf-lua').loclist() end, desc = 'fzf lines' }
-maps.n['zt'] = { function () require('fzf-lua').tabs() end, desc = 'fzf tabs' }
-maps.n['za'] = { function () require('fzf-lua').args() end, desc = 'fzf args' }
-maps.n['zg'] = { function () require('fzf-lua').grep() end, desc = 'fzf grep' }
-maps.n['zgl'] = { function () require('fzf-lua').grep_last() end, desc = 'fzf grep_last' }
-maps.n['zcw'] = { function () require('fzf-lua').cword() end, desc = 'fzf cword' }
-maps.n['zcW'] = { function () require('fzf-lua').cWORD() end, desc = 'fzf cWORD' }
-maps.n['zCW'] = { function () require('fzf-lua').cWORD() end, desc = 'fzf cWORD' }
-maps.n['zgv'] = { function () require('fzf-lua').grep_visual() end, desc = 'fzf grep_visual' }
-maps.n['zgp'] = { function () require('fzf-lua').grep_project() end, desc = 'fzf grep_project' }
-maps.n['zcb'] = { function () require('fzf-lua').grep_curbuf() end, desc = 'fzf grep_curbuf' }
-maps.n['zlcb'] = { function () require('fzf-lua').grep_lcurbuf() end, desc = 'fzf grep_lcurbuf' }
-maps.n['zlg'] = { function () require('fzf-lua').live_grep() end, desc = 'fzf live_grep' }
-maps.n['zlgr'] = { function () require('fzf-lua').live_grep_resume() end, desc = 'fzf live_grep_resume' }
-maps.n['zlgg'] = { function () require('fzf-lua').live_grep_glob() end, desc = 'fzf live_grep_glob' }
-maps.n['zlgn'] = { function () require('fzf-lua').live_grep_glob() end, desc = 'fzf live_grep_native' }
-maps.n['ztag'] = { function () require('fzf-lua').tags() end, desc = 'fzf tags' }
-maps.n['zbtag'] = { function () require('fzf-lua').btags() end, desc = 'fzf btags' }
-maps.n['zbtg'] = { function () require('fzf-lua').tags_grep() end, desc = 'fzf tags_grep' }
-maps.n['zbtgcw'] = { function () require('fzf-lua').tags_grep_cword() end, desc = 'fzf tags_grep_cword' }
-maps.n['ztgcw'] = { function () require('fzf-lua').tags_grep_cword() end, desc = 'fzf tags_grep_cword' }
-maps.n['ztgCW'] = { function () require('fzf-lua').tags_grep_cWORD() end, desc = 'fzf tags_grep_cWORD' }
-maps.n['ztgv'] = { function () require('fzf-lua').tags_grep_visual() end, desc = 'fzf tags_grep_visual' }
-maps.n['ztlg'] = { function () require('fzf-lua').tags_grep_visual() end, desc = 'fzf tags_live_grep' }
-maps.n['zgls'] = { function () require('fzf-lua').git_files() end, desc = 'fzf git_files' }
-maps.n['zgs'] = { function () require('fzf-lua').git_status() end, desc = 'fzf git_files' }
-maps.n['zgc'] = { function () require('fzf-lua').git_commits() end, desc = 'fzf git_commits' }
-maps.n['zgbc'] = { function () require('fzf-lua').git_bcommits() end, desc = 'fzf git_bcommits' }
-maps.n['zgb'] = { function () require('fzf-lua').git_branches() end, desc = 'fzf git_branches' }
-maps.n['zgst'] = { function () require('fzf-lua').git_stash() end, desc = 'fzf git_stash' }
-maps.n['zr'] = { function () require('fzf-lua').resume() end, desc = 'fzf resume' }
-maps.n['zbi'] = { function () require('fzf-lua').builtin() end, desc = 'fzf builtin' }
-maps.n['zht'] = { function () require('fzf-lua').help_tags() end, desc = 'fzf help_tags' }
-maps.n['zm'] = { function () require('fzf-lua').man_pages() end, desc = 'fzf man_pages' }
-maps.n['zc'] = { function () require('fzf-lua').colorschemes() end, desc = 'fzf colorschemes' }
-maps.n['zh'] = { function () require('fzf-lua').highlights() end, desc = 'fzf highlights' }
-maps.n['zcmd'] = { function () require('fzf-lua').commands() end, desc = 'fzf commands' }
-maps.n['zs'] = { function () require('fzf-lua').search_history() end, desc = 'fzf search_history' }
-maps.n['zch'] = { function () require('fzf-lua').changes() end, desc = 'fzf changes' }
-maps.n['zkey'] = { function () require('fzf-lua').keymaps() end, desc = 'fzf keymaps' }
+maps.n['mb'] = { function () require('fzf-lua').buffers() end, desc = 'fzf buffers' }
+maps.n['mp'] = { function () require('fzf-lua').files() end, desc = 'fzf files' }
+maps.n['mh'] = { function () require('fzf-lua').oldfiles() end, desc = 'fzf history' }
+maps.n['mq'] = { function () require('fzf-lua').quickfix() end, desc = 'fzf quickfix' }
+maps.n['mll'] = { function () require('fzf-lua').loclist() end, desc = 'fzf loclist' }
+maps.n['ml'] = { function () require('fzf-lua').loclist() end, desc = 'fzf lines' }
+maps.n['mt'] = { function () require('fzf-lua').tabs() end, desc = 'fzf tabs' }
+maps.n['ma'] = { function () require('fzf-lua').args() end, desc = 'fzf args' }
+maps.n['mg'] = { function () require('fzf-lua').grep() end, desc = 'fzf grep' }
+maps.n['mgl'] = { function () require('fzf-lua').grep_last() end, desc = 'fzf grep_last' }
+maps.n['mcw'] = { function () require('fzf-lua').cword() end, desc = 'fzf cword' }
+maps.n['mcW'] = { function () require('fzf-lua').cWORD() end, desc = 'fzf cWORD' }
+maps.n['mCW'] = { function () require('fzf-lua').cWORD() end, desc = 'fzf cWORD' }
+maps.n['mgv'] = { function () require('fzf-lua').grep_visual() end, desc = 'fzf grep_visual' }
+maps.n['mgp'] = { function () require('fzf-lua').grep_project() end, desc = 'fzf grep_project' }
+maps.n['mcb'] = { function () require('fzf-lua').grep_curbuf() end, desc = 'fzf grep_curbuf' }
+maps.n['mlcb'] = { function () require('fzf-lua').grep_lcurbuf() end, desc = 'fzf grep_lcurbuf' }
+maps.n['mlg'] = { function () require('fzf-lua').live_grep() end, desc = 'fzf live_grep' }
+maps.n['mlgr'] = { function () require('fzf-lua').live_grep_resume() end, desc = 'fzf live_grep_resume' }
+maps.n['mlgg'] = { function () require('fzf-lua').live_grep_glob() end, desc = 'fzf live_grep_glob' }
+maps.n['mlgn'] = { function () require('fzf-lua').live_grep_glob() end, desc = 'fzf live_grep_native' }
+maps.n['mtag'] = { function () require('fzf-lua').tags() end, desc = 'fzf tags' }
+maps.n['mbtag'] = { function () require('fzf-lua').btags() end, desc = 'fzf btags' }
+maps.n['mbtg'] = { function () require('fzf-lua').tags_grep() end, desc = 'fzf tags_grep' }
+maps.n['mbtgcw'] = { function () require('fzf-lua').tags_grep_cword() end, desc = 'fzf tags_grep_cword' }
+maps.n['mtgcw'] = { function () require('fzf-lua').tags_grep_cword() end, desc = 'fzf tags_grep_cword' }
+maps.n['mtgCW'] = { function () require('fzf-lua').tags_grep_cWORD() end, desc = 'fzf tags_grep_cWORD' }
+maps.n['mtgv'] = { function () require('fzf-lua').tags_grep_visual() end, desc = 'fzf tags_grep_visual' }
+maps.n['mtlg'] = { function () require('fzf-lua').tags_grep_visual() end, desc = 'fzf tags_live_grep' }
+maps.n['mgls'] = { function () require('fzf-lua').git_files() end, desc = 'fzf git_files' }
+maps.n['mgs'] = { function () require('fzf-lua').git_status() end, desc = 'fzf git_files' }
+maps.n['mgc'] = { function () require('fzf-lua').git_commits() end, desc = 'fzf git_commits' }
+maps.n['mgbc'] = { function () require('fzf-lua').git_bcommits() end, desc = 'fzf git_bcommits' }
+maps.n['mgb'] = { function () require('fzf-lua').git_branches() end, desc = 'fzf git_branches' }
+maps.n['mgst'] = { function () require('fzf-lua').git_stash() end, desc = 'fzf git_stash' }
+maps.n['mr'] = { function () require('fzf-lua').resume() end, desc = 'fzf resume' }
+maps.n['mbi'] = { function () require('fzf-lua').builtin() end, desc = 'fzf builtin' }
+maps.n['mht'] = { function () require('fzf-lua').help_tags() end, desc = 'fzf help_tags' }
+maps.n['mm'] = { function () require('fzf-lua').man_pages() end, desc = 'fzf man_pages' }
+maps.n['mc'] = { function () require('fzf-lua').colorschemes() end, desc = 'fzf colorschemes' }
+maps.n['mh'] = { function () require('fzf-lua').highlights() end, desc = 'fzf highlights' }
+maps.n['mcmd'] = { function () require('fzf-lua').commands() end, desc = 'fzf commands' }
+maps.n['ms'] = { function () require('fzf-lua').search_history() end, desc = 'fzf search_history' }
+maps.n['mch'] = { function () require('fzf-lua').changes() end, desc = 'fzf changes' }
+maps.n['mkey'] = { function () require('fzf-lua').keymaps() end, desc = 'fzf keymaps' }
 
 -- harpoon
 maps.n['<leader>ha'] = { function () require('harpoon.mark').add_file() end, desc = 'harpoon add_file' }
 maps.n['<leader>hm'] = { function () require('harpoon.ui').toggle_quick_menu() end, desc = 'harpoon toggle_quick_menu' }
 maps.n['<leader>ho'] = { function () require('harpoon.ui').nav_prev() end, desc = 'harpoon nav_prev' }
 maps.n['<leader>hi'] = { function () require('harpoon.ui').nav_next() end, desc = 'harpoon nav_next' }
+
+maps.n['mi'] = { '<cmd>setlocal foldmethod=indent<cr>', desc = 'foldmethod indent' }
 
 astronvim.set_mappings(astronvim.user_plugin_opts("mappings", maps))
